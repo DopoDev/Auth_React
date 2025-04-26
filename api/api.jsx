780bf9ce3527d8b4387fc36ddcc6240ff8c357ae
Johao Ramirez <johaoramirez1234@gmail.com>
2025-04-25 20:16:02 -0500

    api

--- api/api.jsx
+++ api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/', 
+    baseURL: 'https://mrarrieta-auth.onrender.com/', 
     header: {
         'Content-Type': 'application/json',
     },
